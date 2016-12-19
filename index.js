aa14462486834ee3f149fdfbadd23c26a7cfd1e3
Kartik Gola <Kartik Gola>
2016-12-20 01:03:47 +0530

    Finally PORT fixed!

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(process.env.port || 3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server started...");
 });

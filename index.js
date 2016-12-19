6760bbd400f5d96b47179649a329ac413e996aa2
Kartik Gola <Kartik Gola>
2016-12-20 00:40:34 +0530

    Initial

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    res.json({
+        "IP" : req.ip,
+        "Language" : req.get('accept-language').split(',')[0],
+        "Operating System" : req.get('user-agent').split(/\(|\)/g)[1],
+    });
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(3000, function(){
+    console.log("Now listening on port 3000");
+});
